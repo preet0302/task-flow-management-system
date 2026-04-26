11a9d7412ace20a0e9af03cf0a83bca7b1e52842
preet0302 <preetpraja1234@gmail.com>
2026-04-26 22:33:19 +0530

    Add proxy configuration for API requests

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://task-backend-yagh.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
